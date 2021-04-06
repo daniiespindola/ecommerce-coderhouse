70b5ee55396dc3ef2e7468effc844456b25368b0
Daniel Espindola <riverplate.dani33@gmail.com>
2021-04-05 22:42:47 -0300

    modificaciones clase 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/Navbar';
-import ItemListContainer from './components/ItemListContainer';
+import NavBar from './components/Navbar.js';
+import ItemListContainer from './components/ItemListContainer.js';
 
 function App() {
   return (
     <>
-    <h1>Surf Stuff</h1>
     <div className="App">
       <NavBar />
-      <ItemListContainer greeting="Bienvenidos" />
     </div>
+    <ItemListContainer greeting="Bienvenidos" />
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import CartWidget from './CartWidget'
+import CartWidget from './CartWidget.js'
 import './Navbar.css'
 
 function Navbar(props) {
@@ -8,7 +8,7 @@ function Navbar(props) {
               <ul id="nav">
                 <li><a href="">Home</a></li>
                 <li><a href="">About</a></li>
-                <li><a href="">FAQ</a></li>
+                <li><a href="">Products</a></li>
                 <li><a href="">Contact</a></li>
                 <li><a style={{marginTop: '-5px'}} href=""><CartWidget /></a></li>
               </ul>
