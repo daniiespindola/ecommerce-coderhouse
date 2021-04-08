41b2040614af120d8e431d49730cbef8d8755cd7
Daniel Espindola <riverplate.dani33@gmail.com>
2021-04-07 23:18:39 -0300

    contador

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/Navbar.js';
 import ItemListContainer from './components/ItemListContainer.js';
+import ItemCount from './components/ItemCount';
 
-function App() {
+
+const App = () =>{
   return (
     <>
     <div className="App">
       <NavBar />
     </div>
     <ItemListContainer greeting="Bienvenidos" />
+          <ItemCount stock={100} count={1}/>
+
     </>
   );
 }
---
+++ src/components/ItemCount.js
@@ -0,0 +1,37 @@
+import {Button} from 'react-bootstrap'
+import {useState} from 'react'
+
+const ItemCount = ({initialCount, stock}) => {
+
+    const [count, setCount] = useState(initialCount ? initialCount : 0);
+
+    const increment = () => {
+        if (count === stock){
+            return;
+        }
+        setCount(count + 1);
+    }
+    const decrement = () => {
+        if (count === 0){
+            return;
+        }
+        setCount(count - 1);
+    }
+
+        return(
+            <>
+                <div className="counter d-flex flex-wrap mx-auto">
+                    <div className="count">
+                        <span>{count}</span>
+                    </div>
+                    <div className="d-flex btn-counter">
+                        <Button variant='light' className='font-size-2' onClick={decrement}>-</Button>
+                        <Button variant='light' className='font-size-2' onClick={increment}>+</Button>
+                    </div>
+                </div>
+            </>
+        )
+    
+}
+
+export default ItemCount;
