02ed5eedc6a6599267da4be832ba484a528f5c60
Daniel Espindola <riverplate.dani33@gmail.com>
2021-05-04 00:02:43 -0300

    se saca imagen de nav, se agrega icono de carrito y demas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import NavBar from "./components/NavBar/NavBar"
+import NavBar from "./components/NavBar/NavBar.js"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,5 @@
 
 import React, {useState, useEffect} from 'react'
-import "./ItemListContainer.css"
 import ItemCount from "../ItemCount/ItemCount"
 import ItemList from "../ItemList/ItemList"
 
